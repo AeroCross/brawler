ade264888e15830fb359ac89fdf1b34d820c078c
Mario Cuba <mario.cuba@envato.com>
2016-07-11 09:31:57 +1000

    And here!

--- spec/brawler/finder/ability_finder_spec.rb
+++ spec/brawler/finder/ability_finder_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "brawler/finder/ability_finder"
 
 RSpec.describe Brawler::Finder::AbilityFinder do
-  describe "class methods" do
+  describe "Class methods" do
     describe ".for_hero" do
       # This should accept both raw data or a Brawler::Hero instance
       it "returns an array all the abilities for a given hero"
