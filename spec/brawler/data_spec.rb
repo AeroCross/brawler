87fb49073edddca7a30beace870cf2dcc6f42168
Mario Cuba <mario.cuba@envato.com>
2016-07-10 15:24:32 +1000

    Brawler::Data didn't have tests... until now

--- spec/brawler/data_spec.rb
+++ spec/brawler/data_spec.rb
@@ -3,8 +3,21 @@ require "brawler/data"
 
 RSpec.describe Brawler::Data do
   describe "class methods" do
-    subject(:test) { described_class.test }
+    describe ".load" do
+      subject(:heroes_data) { described_class.load }
 
-      it "loads the Heroes JSON data"
+      it "loads the Heroes JSON data" do
+        expect(heroes_data).to be_kind_of(Enumerable)
+      end
+
+      it "has the expected data" do
+        expect(heroes_data.first).to include(:name)
+        expect(heroes_data.first).to include(:title)
+        expect(heroes_data.first).to include(:ratings)
+        expect(heroes_data.first).to include(:stats)
+        expect(heroes_data.first).to include(:abilities)
+        expect(heroes_data.first).to include(:talents)
+      end
+    end
   end
 end
