dfd87b8a024e2d02b759c37f3888f5869df46722
Mario Cuba <mario.cuba@envato.com>
2016-07-10 16:06:13 +1000

    Use appropriate method name + make sure it returns nil when it should!

--- spec/brawler/finder/hero_finder_spec.rb
+++ spec/brawler/finder/hero_finder_spec.rb
@@ -2,12 +2,23 @@ require "spec_helper"
 require "brawler/finder/hero_finder"
 
 RSpec.describe Brawler::Finder::HeroFinder do
+  let(:hero) { "Thrall" }
+
   describe "class methods" do
-    describe ".find_by_name" do
-      subject(:find_by_name) { described_class.find_by_name("Thrall") }
+    describe ".by_name" do
+      subject(:by_name) { described_class.by_name(hero) }
 
       it "returns information of a Hero" do
-        expect(find_by_name.title).to eq("Warchief of the Horde")
+        expect(by_name.name).to eq("Thrall")
+        expect(by_name.title).to eq("Warchief of the Horde")
+      end
+
+      context "when the hero does not exist" do
+        let(:hero) { "Lothar" }
+
+        it "returns nil" do
+          expect(by_name).to eq(nil)
+        end
       end
     end
   end
