32a9e310e69136ffdaba40141b0ed676106e693e
Mario Cuba <mario.cuba@envato.com>
2016-07-10 16:09:12 +1000

    Add more context

--- spec/brawler/finder/hero_finder_spec.rb
+++ spec/brawler/finder/hero_finder_spec.rb
@@ -3,14 +3,17 @@ require "brawler/finder/hero_finder"
 
 RSpec.describe Brawler::Finder::HeroFinder do
   let(:hero) { "Thrall" }
+  let(:role) { "Support" }
 
   describe "class methods" do
     describe ".by_name" do
       subject(:by_name) { described_class.by_name(hero) }
 
-      it "returns information of a Hero" do
-        expect(by_name.name).to eq("Thrall")
-        expect(by_name.title).to eq("Warchief of the Horde")
+      context "when the Hero exists" do
+        it "returns information of a Hero" do
+          expect(by_name.name).to eq("Thrall")
+          expect(by_name.title).to eq("Warchief of the Horde")
+        end
       end
 
       context "when the hero does not exist" do
